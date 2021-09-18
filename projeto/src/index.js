679a67473bd50cddb9e635d5a6bbe73505ec8d1b
TijonSilva <gabrieltijon@gmail.com>
2021-09-17 21:07:02 -0300

    listando projetos da minha API

--- projeto/src/index.js
+++ projeto/src/index.js
@@ -1,13 +1,32 @@
-import React from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native"
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from "react-native"
+
+import api from './services/api'
 
 export default function App() {
+ const [projects, setProjects] = useState([])
+
+ useEffect(() => {
+  api.get('projects').then(response => {
+   console.log(response.data)
+   setProjects(response.data)
+  })
+ }, [])
+
  return (
   <>
    <StatusBar barStyle="light-content" backgroundColor='#7159c1' />
-   <View style={styles.container}>
-    <Text style={styles.title}> Hellow RactNative</Text>
-   </View>
+
+   <SafeAreaView style={styles.container}>
+    <FlatList
+     data={projects}
+     keyExtractor={project => project.id}
+     renderItem={({ item: project }) => (
+      <Text style={styles.project}>{project.title}</Text>
+
+     )}
+    />
+   </SafeAreaView>
   </>
  );
 }
@@ -16,13 +35,10 @@ const styles = StyleSheet.create({
  container: {
   backgroundColor: '#7159c1',
   flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center'
  },
 
- title: {
+ project: {
   color: '#FFF',
-  fontSize: 42,
-  fontWeight: 'bold'
+  fontSize: 30,
  }
 })
